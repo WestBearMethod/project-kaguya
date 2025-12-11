ff557fd3fe10f0879f8103a1a77a0f6cd30cebee
WestBearMethod <248833206+WestBearMethod@users.noreply.github.com>
2025-12-11 09:38:26 +0000

    (#3) feat: アプリケーションサーバーが正常に動作しているか確認するための/health エンドポイントを作成した。
    以下の URL で指摘された。
    https://github.com/WestBearMethod/project-kaguya/pull/4#discussion_r2608038471

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { openapi } from "@elysiajs/openapi";
 import { JSONSchema } from "effect";
 import { Elysia } from "elysia";
 import { descriptionController } from "./infrastructure/description/description";
+import { healthController } from "./infrastructure/health/health";
 
 const app = new Elysia();
 
@@ -17,6 +18,7 @@ if (process.env.NODE_ENV === "development") {
 
 app
   .get("/", () => "Hello Elysia")
+  .use(healthController)
   .use(descriptionController)
   .listen(3000);
 

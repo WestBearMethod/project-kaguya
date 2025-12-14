47dfde1a149f9e46f31e5028c37b754c7296ec78
WestBearMethod <248833206+WestBearMethod@users.noreply.github.com>
2025-12-14 17:11:40 +0000

    (#17) fix: マイグレーションファイルに関して新しいインデックスを作成するまで低速になるようなので 1 から作り直させた。
    以下の URL で指摘された。
    https://github.com/WestBearMethod/project-kaguya/pull/20#discussion_r2617282369
    
    本番環境に投入していないのでこれでいいと思われる。

--- drizzle/0002_greedy_colleen_wing.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "descriptions" ADD COLUMN "deleted_at" timestamp;
--- drizzle/0005_whole_vertigo.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX "channel_id_created_at_idx";--> statement-breakpoint
-ALTER TABLE "descriptions" ADD COLUMN "category" varchar(20);--> statement-breakpoint
-CREATE INDEX "channel_id_category_created_at_idx" ON "descriptions" USING btree ("channel_id","category","created_at","id");
